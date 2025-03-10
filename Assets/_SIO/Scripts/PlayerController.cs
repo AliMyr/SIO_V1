eb0f77797dd40405cec4cdf4cb8bead9ec7d8030
AliMyr <rabo41ali@gmail.com>
2025-03-10 16:29:46 +0500

    (ADD) PlayerController

---
+++ Assets/_SIO/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    public Joystick joystick;
+    private CharacterController controller;
+
+    public bool joystickActive = false;
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        float horizontalInput = 0;
+        float verticalInput = 0;
+        if (joystickActive)
+        {
+            horizontalInput = joystick.Horizontal;
+            verticalInput = joystick.Vertical;
+        }
+        else
+        {
+           horizontalInput = Input.GetAxis("Horizontal"); 
+           verticalInput = Input.GetAxis("Vertical");
+        }
+
+        Vector3 moveDirection = transform.forward * verticalInput + transform.right * horizontalInput;
+        
+        moveDirection.y -= 9.81f * Time.deltaTime;
+        
+        controller.Move(moveDirection * moveSpeed * Time.deltaTime);
+    }
+}
