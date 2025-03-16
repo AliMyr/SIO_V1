c316fe012a01729eb067c2130edab3006172a0bd
AliMyr <rabo41ali@gmail.com>
2025-03-17 00:21:53 +0500

    (TEST) Health Component

---
+++ Assets/_SIO/Scripts/Character.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    public abstract void Initialize();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/_SIO/Scripts/Components/Health/HealthComponent.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class HealthComponent : IHealthComponent
+{
+    private float health = 25;
+    private float maxHealth = 100;
+
+    public float Health 
+    {
+        get 
+        {
+            return health;
+        }
+        private set 
+        {
+            health = Mathf.Clamp(value, 0, MaxHealth);
+            if (health == 0)
+            {
+                SetDeath();
+            }
+        }
+    }
+
+
+    public float MaxHealth 
+    {
+        get 
+        {
+            return maxHealth; 
+        }   
+    }
+
+    public void SetDamage(int damage)
+    {
+        Health -= damage;
+    }
+
+    private void SetDeath() 
+    {
+        Debug.Log("Death");
+    }
+}
