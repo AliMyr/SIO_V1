c07b34c0bbcf8cfaacb79d584993063668359b1e
AliMyr <rabo41ali@gmail.com>
2025-03-21 21:46:30 +0500

    (TEST) test

--- Assets/_SIO/Scripts/Character/Components/Damage/CharacterDamgaeComponent.cs
+++ Assets/_SIO/Scripts/Character/Components/Damage/CharacterDamgaeComponent.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class CharacterDamageComponent : IDamageComponent
 {
     public float Damage => 10;
--- Assets/_SIO/Scripts/Character/Components/Damage/IDamageComponent.cs
+++ Assets/_SIO/Scripts/Character/Components/Damage/IDamageComponent.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public interface IDamageComponent
 {
     float Damage {  get; }
